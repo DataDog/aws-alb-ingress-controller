5b1149e3de556dd276ba49e749e8a530a45aefcb
Peter Rifel <peter.rifel@datadoghq.com>
2021-03-22 11:50:49 -0500

    Include NLB and TG tags for migration to aws-load-balancer-controller

--- internal/nlb/generator/tag.go
+++ internal/nlb/generator/tag.go
@@ -1,6 +1,8 @@
 package generator
 
 import (
+	"fmt"
+
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/nlb/lb"
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/nlb/tg"
 )
@@ -10,6 +12,14 @@ const (
 	TagKeyNamespace   = "kubernetes.io/namespace"
 	TagKeyServiceName = "kubernetes.io/service-name"
 	TagKeyServicePort = "kubernetes.io/service-port"
+
+	// These are tags that the upstream aws-load-balancer-controller expects on NLBs and TargetGroups
+	// Our aws-alb-ingress-controller fork adds these tags to ease the migration back to upstream
+	// ref:
+	// https://github.com/kubernetes-sigs/aws-load-balancer-controller/blob/a30c980164db2d9f54596ab971be8d98453c679d/pkg/deploy/tracking/provider.go#L11-L24
+	TagKeyLBCServiceResource = "service.k8s.aws/resource"
+	TagKeyLBCCluster         = "elbv2.k8s.aws/cluster"
+	TagKeyLBCStack           = "service.k8s.aws/stack"
 )
 
 var _ tg.TagGenerator = (*TagGenerator)(nil)
@@ -21,17 +31,20 @@ type TagGenerator struct {
 }
 
 func (gen *TagGenerator) TagLB(namespace string, serviceName string) map[string]string {
-	return gen.tagServiceResources(namespace, serviceName)
+	t := gen.tagServiceResources(namespace, serviceName)
+	t[TagKeyLBCServiceResource] = "LoadBalancer"
+	return t
 }
 
 func (gen *TagGenerator) TagTGGroup(namespace string, serviceName string) map[string]string {
 	return gen.tagServiceResources(namespace, serviceName)
 }
 
-func (gen *TagGenerator) TagTG(serviceName string, servicePort string) map[string]string {
+func (gen *TagGenerator) TagTG(namespace, serviceName string, servicePort string) map[string]string {
 	return map[string]string{
-		TagKeyServiceName: serviceName,
-		TagKeyServicePort: servicePort,
+		TagKeyServiceName:        serviceName,
+		TagKeyServicePort:        servicePort,
+		TagKeyLBCServiceResource: fmt.Sprintf("%s/%s:%d", namespace, serviceName, servicePort),
 	}
 }
 
@@ -43,5 +56,7 @@ func (gen *TagGenerator) tagServiceResources(namespace string, serviceName strin
 	m["kubernetes.io/cluster/"+gen.ClusterName] = "owned"
 	m[TagKeyNamespace] = namespace
 	m[TagKeyServiceName] = serviceName
+	m[TagKeyLBCCluster] = gen.ClusterName
+	m[TagKeyLBCStack] = fmt.Sprintf("%s/%s", namespace, serviceName)
 	return m
 }
